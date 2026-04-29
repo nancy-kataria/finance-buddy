160f222c79783a32e78686063d9da195ef0a2a00
nancy-kataria <katarianancy8@gmail.com>
2026-04-29 01:16:05 -0700

    added auth header to server client

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,27 +1,31 @@
-import { createServerClient } from '@supabase/ssr'
-import { cookies } from 'next/headers'
+import { createServerClient } from "@supabase/ssr";
+import { cookies, headers } from "next/headers";
 
 export async function createClient() {
-  const cookieStore = await cookies()
+  const cookieStore = await cookies();
+  const authHeader = (await headers()).get("Authorization");
 
   return createServerClient(
     process.env.SUPABASE_URL!,
     process.env.SUPABASE_ANON_KEY!,
     {
+      global: {
+        headers: authHeader ? { Authorization: authHeader } : {},
+      },
       cookies: {
         getAll() {
-          return cookieStore.getAll()
+          return cookieStore.getAll();
         },
         setAll(cookiesToSet) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            )
+              cookieStore.set(name, value, options),
+            );
           } catch {
             //this catch is expected when calling from a server component, that is already rendering.
           }
         },
       },
-    }
-  )
+    },
+  );
 }
